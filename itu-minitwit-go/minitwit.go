bd2d0dd2b9477aa974e48f4900d5febfe2684646
Alma Freiesleben <alma.rosager@gmail.com>
2021-02-08 14:41:34 +0100

    refactor-6: Added sceleton for api endpoints add_message and unfollow user in go

--- itu-minitwit-go/minitwit.go
+++ itu-minitwit-go/minitwit.go
@@ -1,1 +1,24 @@
-Here we make the minitwit golang implementation
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/{username}/unfollow", unfollow_user)
+	r.HandleFunc("//add_message", add_message).Methods("POST")
+
+	http.ListenAndServe(":80", r)
+}
+
+func unfollow_user(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func add_message(w http.ResponseWriter, r *http.Request) {
+
+}
