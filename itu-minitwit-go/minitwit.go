ff0918ef7ae2755bb63e4d7590f8125de478dcad
Alma Freiesleben <alma.rosager@gmail.com>
2021-02-08 19:21:42 +0100

    refactor-6: Added pseudo-code for the two methods plus some signatures for helper functions and session code (that doesn't work)

--- itu-minitwit-go/minitwit.go
+++ itu-minitwit-go/minitwit.go
@@ -4,12 +4,16 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var db *sql.DB
+var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY"))) // TODO
 
 func main() {
 	r := mux.NewRouter()
@@ -53,12 +57,27 @@ func unfollow_user(w http.ResponseWriter, r *http.Request) {
 
 	db := get_db()
 	db.Exec("delete from follower where who_id=? and whom_id=?", get_user_id(user), whom_id)
-	msg := []byte(fmt.Sprintf("You are no longer following %s", username))
-	w.Write(msg)
+	successMessage := []byte(fmt.Sprintf("You are no longer following %s", username))
+	w.Write(successMessage)
 	http.Redirect(w, r, fmt.Sprintf("/%v", vars["username"]), 302)
 }
 
 // Registers a new message for the user.
 func add_message(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session-name")
+	user_id := session.Values["user_id"]
 
+	if user_id == nil {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+
+	message := r.FormValue("text")
+	if len(message) > 0 {
+		db := get_db()
+		db.Exec("insert into message (author_id, text, pub_date, flagged) values (?, ?, ?, 0)", user_id, message, time.Now())
+	}
+	successMessage := []byte("Your message was recorded")
+	w.Write(successMessage)
+
+	http.Redirect(w, r, "timeline", 302)
 }
